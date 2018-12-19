c81c94a67c62e8fab3fd3c0dff07b2feb0ee7262
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2018-12-20 02:11:55 +0900

    改行だけの引用があると「レスへの返信を探す」が正常に表示されない不具合を修正

--- akaan/res.js
+++ akaan/res.js
@@ -196,6 +196,8 @@ class Quote {
         let search_text = this.green_text.innerText;
         search_text = search_text.slice(1).replace(/^[\s]+|[\s]+$/g, "");
 
+        if (!search_text.length) return -1;
+
         for (let i = this.index - 1; i >= 0; --i) {
             let target = search_targets[i];
 
